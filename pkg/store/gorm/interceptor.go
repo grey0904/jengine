3e2eb9431b5b9b5e5b7dd52a95e391ffe7a736b9
grey <grey_229@gmail.com>
2023-12-07 11:35:18 +0800

    真正的初始化

---
+++ pkg/store/gorm/interceptor.go
@@ -0,0 +1,134 @@
+// Copyright 2022 Douyu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gorm
+
+import (
+	"errors"
+	"time"
+
+	"github.com/alibaba/sentinel-golang/core/base"
+	prome "github.com/grey0904/jengine/pkg/core/metric"
+	"github.com/grey0904/jengine/pkg/core/sentinel"
+	"github.com/grey0904/jengine/pkg/core/xtrace"
+	"github.com/grey0904/jengine/pkg/xlog"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc/metadata"
+	"gorm.io/gorm"
+)
+
+type Handler func(*gorm.DB)
+type Interceptor func(dsn *DSN, op string, options *Config, next Handler) Handler
+
+var errSlowCommand = errors.New("mysql slow command")
+
+func metricInterceptor() Interceptor {
+	return func(dsn *DSN, op string, options *Config, next Handler) Handler {
+		return func(scope *gorm.DB) {
+			beg := time.Now()
+			next(scope)
+			cost := time.Since(beg)
+
+			// error metric
+			if scope.Error != nil {
+				prome.LibHandleCounter.WithLabelValues(prome.TypeMySQL, dsn.DBName+"."+scope.Name(), dsn.Addr, getStatement(scope.Error.Error())).Inc()
+
+				if scope.Error != gorm.ErrRecordNotFound {
+					xlog.Jupiter().Error("mysql err", xlog.FieldErr(scope.Error), xlog.FieldName(dsn.DBName+"."+scope.Name()), xlog.FieldMethod(op))
+				} else {
+					xlog.Jupiter().Warn("record not found", xlog.FieldErr(scope.Error), xlog.FieldName(dsn.DBName+"."+scope.Name()), xlog.FieldMethod(op))
+				}
+			} else {
+				prome.LibHandleCounter.WithLabelValues(prome.TypeMySQL, dsn.DBName+"."+scope.Name(), dsn.Addr, "OK").Inc()
+			}
+
+			prome.LibHandleHistogram.WithLabelValues(prome.TypeMySQL, dsn.DBName+"."+scope.Name(), dsn.Addr).Observe(cost.Seconds())
+
+			if options.SlowThreshold > time.Duration(0) && options.SlowThreshold < cost {
+				xlog.Jupiter().Error(
+					"slow",
+					xlog.FieldErr(errSlowCommand),
+					xlog.FieldMethod(op),
+					xlog.FieldExtMessage(logSQL(scope.Statement.SQL.String(), scope.Statement.Vars, options.DetailSQL)),
+					xlog.FieldAddr(dsn.Addr),
+					xlog.FieldName(dsn.DBName+"."+scope.Name()),
+					xlog.FieldCost(cost),
+				)
+			}
+		}
+	}
+}
+
+func logSQL(sql string, args []interface{}, containArgs bool) string {
+	if containArgs {
+		return bindSQL(sql, args)
+	}
+	return sql
+}
+
+func traceInterceptor() Interceptor {
+	tracer := xtrace.NewTracer(trace.SpanKindClient)
+	attrs := []attribute.KeyValue{
+		semconv.RPCSystemKey.String("gorm"),
+	}
+
+	return func(dsn *DSN, op string, options *Config, next Handler) Handler {
+		return func(scope *gorm.DB) {
+
+			if ctx := scope.Statement.Context; ctx != nil {
+				md := metadata.New(nil)
+
+				_, span := tracer.Start(ctx, op, propagation.HeaderCarrier(md), trace.WithAttributes(attrs...))
+
+				span.SetAttributes(semconv.DBNameKey.String(dsn.DBName))
+				span.SetAttributes(semconv.DBConnectionStringKey.String(dsn.Addr))
+				span.SetAttributes(semconv.DBUserKey.String(dsn.User))
+				span.SetAttributes(semconv.DBStatementKey.String(
+					logSQL(scope.Statement.SQL.String(), scope.Statement.Vars, options.DetailSQL)))
+
+				defer span.End()
+
+				next(scope)
+
+				return
+			}
+
+			next(scope)
+
+		}
+	}
+}
+
+func sentinelInterceptor() Interceptor {
+	return func(dsn *DSN, op string, options *Config, next Handler) Handler {
+		return func(scope *gorm.DB) {
+			entry, blockerr := sentinel.Entry(dsn.Addr,
+				sentinel.WithResourceType(base.ResTypeDBSQL),
+				sentinel.WithTrafficType(base.Outbound),
+			)
+			if blockerr != nil {
+				_ = scope.AddError(blockerr)
+
+				return
+			}
+
+			next(scope)
+
+			entry.Exit(sentinel.WithError(scope.Error))
+		}
+	}
+}
---
+++ pkg/store/mongox/option.go
@@ -0,0 +1,68 @@
+package mongox
+
+import (
+	"time"
+
+	"github.com/grey0904/jengine/pkg/conf"
+	"github.com/grey0904/jengine/pkg/core/constant"
+	"github.com/grey0904/jengine/pkg/core/ecode"
+	"github.com/grey0904/jengine/pkg/core/singleton"
+	"github.com/grey0904/jengine/pkg/xlog"
+	"github.com/spf13/cast"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
+)
+
+// Config ...
+type (
+	Config struct {
+		Name string
+		// DSN地址
+		DSN string `json:"dsn" toml:"dsn"`
+		// 创建连接的超时时间
+		SocketTimeout time.Duration `json:"socketTimeout" toml:"socketTimeout"`
+		// 连接池大小(最大连接数)
+		PoolLimit int `json:"poolLimit" toml:"poolLimit"`
+
+		logger *zap.Logger
+	}
+)
+
+// StdConfig 返回标准配置
+func StdConfig(name string) *Config {
+	return RawConfig(constant.ConfigKey("mongo." + name))
+}
+
+// RawConfig 裸配置
+func RawConfig(key string) *Config {
+	var config = DefaultConfig()
+	config.Name = key
+	if err := conf.UnmarshalKey(key, &config, conf.TagName("toml")); err != nil {
+		panic(err)
+	}
+	return config
+}
+
+// DefaultConfig 默认配置
+func DefaultConfig() *Config {
+	return &Config{
+		SocketTimeout: cast.ToDuration("5s"),
+		PoolLimit:     100,
+		logger:        xlog.Jupiter().Named(ecode.ModStoreMongo),
+	}
+}
+
+func (config *Config) Build() *mongo.Client {
+	return newSession(config)
+}
+
+func (config *Config) Singleton() *mongo.Client {
+	if val, ok := singleton.Load(constant.ModuleStoreMongoDB, config.Name); ok {
+		return val.(*mongo.Client)
+	}
+
+	val := config.Build()
+	singleton.Store(constant.ModuleStoreMongoDB, config.Name, val)
+
+	return val
+}
